6a92518a5fd5421eea84808b9eb773411b363867
kidagine <davidkalatzis1999@hotmail.com>
2021-02-10 21:18:38 +0100

    Initial commit of new design.

--- main.js
+++ main.js
@@ -2,13 +2,13 @@
 // When the user scrolls down 20px from the top of the document, slide down the navbar
 window.onscroll = function() {scrollFunction()};
 
-function scrollFunction() {
-    if (document.body.scrollTop > 1|| document.documentElement.scrollTop > 1) {
-    document.getElementById("header").style.top = "0";
-    } else {
-    document.getElementById("header").style.top = "-50px";
-    }
-}
+// function scrollFunction() {
+//     if (document.body.scrollTop > 1|| document.documentElement.scrollTop > 1) {
+//     document.getElementById("navbar").style.left = "0";
+//     } else {
+//     document.getElementById("navbar").style.left = "-65px";
+//     }
+// }
 
 $('.barra-nivel').each(function() {
     var valorLargura = $(this).data('nivel');
